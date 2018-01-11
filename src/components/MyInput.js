422c01d960d4efee879fa82ae275ff3e1e5c1ce4
Rabinovich Irina <webmaza75@gmail.com>
2018-01-11 23:07:30 +0300

    refactoring MyInput

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -3,17 +3,12 @@ import React from 'react'
 class MyInput extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { text: this.props.text };
     }
-
-    handleChange = (e) => {
-        this.setState({ text: e.target.value });
-    };
 
     render() {
         return (<div className='input__group'>
             <label className='input__label' htmlFor={this.props.name} >{this.props.label}</label>
-            <input className='input__field'  id={this.props.name} placeholder={this.props.placeholder} onChange={this.handleChange} value={this.state.text} />
+            <input className='input__field'  id={this.props.name} placeholder={this.props.placeholder} value={this.props.value} onChange={this.props.change} />
             </div>
         );
     }
