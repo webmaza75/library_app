253b7e403208a2afb2468d360d77d41685f9ad57
Rabinovich <irina.rabinovich@accenture.com>
2018-01-15 17:12:26 +0300

    import css file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import LibraryBox from './components/LibraryBox.js'
 
+import './css/style.css';
+
 const listItems = [
     {title: 'title1', author: 'author1', year: 'year1'},
     {title: 'title2', author: 'author2', year: 'year2'}
