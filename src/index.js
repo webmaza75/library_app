33b44d2b8bbef350c06b4ddcecfd0a034a6f519a
Rabinovich Irina <webmaza75@gmail.com>
2018-01-16 00:09:42 +0300

    add id into the book

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import LibraryBox from './components/LibraryBox.js'
 import './css/style.css';
 
 const listItems = [
-    {title: 'title1', author: 'author1', year: 'year1'},
-    {title: 'title2', author: 'author2', year: 'year2'}
+    {id: 1, title: 'title1', author: 'author1', year: 'year1'},
+    {id: 2, title: 'title2', author: 'author2', year: 'year2'}
 ];
 
 render(
